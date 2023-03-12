5acd1191413784cce78d08f625e191cac86ca161
police <police@state.gov>
2023-03-12 08:47:49 +0200

    supress log msgs

--- lottery.py
+++ lottery.py
@@ -6,7 +6,7 @@ from datetime import timedelta
 from pid import PID
 
 class DarkfiTable:
-    def __init__(self, airdrop, running_time, controller_type=CONTROLLER_TYPE_DISCRETE, kp=0, ki=0, kd=0, dt=1, target=1, kc=0, ti=0, td=0, ts=0):
+    def __init__(self, airdrop, running_time, controller_type=CONTROLLER_TYPE_DISCRETE, kp=0, ki=0, kd=0, dt=1, target=1, kc=0, ti=0, td=0, ts=0, debug=False):
         self.Sigma=airdrop
         self.darkies = []
         self.running_time=running_time
@@ -15,11 +15,13 @@ class DarkfiTable:
         self.pid = None
         self.pid = PID(kp=kp, ki=ki, kd=kd, dt=dt, target=target, Kc=kc, Ti=ti, Td=td, Ts=ts)
         self.controller_type=controller_type
+        self.debug=debug
 
     def add_darkie(self, darkie):
         self.darkies+=[darkie]
 
-    def background(self, rand_running_time=True, debug=True):
+    def background(self, rand_running_time=True, debug=False):
+        self.debug=debug
         self.start_time=time.time()
         feedback=0 # number leads in previous slot
         count = 0
@@ -28,13 +30,13 @@ class DarkfiTable:
         self.running_time = rand_running_time
         if rand_running_time and debug:
             print("random running time: {}".format(self.running_time))
+            print('running time: {}'.format(self.running_time))
         spinoffs = []
         joins = []
         finalizations = []
         restarts = []
         pids = []
         length = len(self.darkies)
-        print('running time: {}'.format(self.running_time))
         while count < self.running_time:
             winners = 0
 
@@ -114,5 +116,6 @@ class DarkfiTable:
 
     def write(self):
         elapsed=self.end_time-self.start_time
-        print("total time: {}, slot time: {}".format(str(timedelta(seconds=elapsed)), str(timedelta(seconds=elapsed/self.running_time))))
+        if self.debug:
+            print("total time: {}, slot time: {}".format(str(timedelta(seconds=elapsed)), str(timedelta(seconds=elapsed/self.running_time))))
         self.pid.write()

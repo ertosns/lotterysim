4b7e06b94bee8809dd3face0ef1b30389d06d28b
police <police@state.gov>
2023-03-14 15:28:06 +0200

    auto crawler

--- instance.py
+++ instance.py
@@ -8,12 +8,12 @@ RUNNING_TIME = int(input("running time:"))
 
 if __name__ == "__main__":
     darkies = []
-    darkies += [ Darkie(random.gauss(20,20)*50) for id in range(300) ]
+    darkies += [ Darkie(random.gauss(20,20)*50) for id in range(100) ]
     airdrop = 0
     for darkie in darkies:
         airdrop+=darkie.stake
     print("network airdrop: {} on {} nodes".format(airdrop, len(darkies)))
-    dt = DarkfiTable(airdrop, RUNNING_TIME, CONTROLLER_TYPE_DISCRETE, kp=0.24, ki=0.02, kd=-0.1)
+    dt = DarkfiTable(airdrop, RUNNING_TIME, CONTROLLER_TYPE_DISCRETE, kp=0.18, ki=0.02, kd=-0.1)
     for darkie in darkies:
         dt.add_darkie(darkie)
     acc = dt.background(rand_running_time=False)
